d9a4c99cb76a34c76c29f42974f6f5694e13ef10
Jose Del Vecchio <jose.delvecchio@amerikatis.com>
2019-05-03 17:32:30 -0500

    routing mensaje

--- src/app/mensaje/mensaje-routing.module.ts
+++ src/app/mensaje/mensaje-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComponentMensajeComponent } from './component-mensaje/component-mensaje.component'
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'mensaje',
+  component: ComponentMensajeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class MensajeRoutingModule { }
